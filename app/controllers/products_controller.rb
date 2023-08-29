ae654a303dc274662cce465e3f08e8b0067a7872
Elisa Herrera <elisaherrera903@gmail.com>
2023-08-29 14:08:39 -0600

    creamos edit, delete y show de products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,10 @@
 class ProductsController < ApplicationController
+  before_action :set_product, only: [:show, :edit, :update, :destroy]
   def index
     @products = Product.all
   end
 
   def show
-    @product = Product.find(params[:id])
   end
 
   def new
@@ -21,18 +21,39 @@ class ProductsController < ApplicationController
     end
   end
 
+  def my_products
+    @products = Product.where(user: current_user)
+  end
+
+  # def category
+  #   @category = [params[products][category]]
+  #   @products = Product.where(category: )
+  # end
+
   def edit
   end
 
   def update
+    @product.update(product_params)
+    if @product.save
+      redirect_to product_path(@product)
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @product.destroy
+    redirect_to my_products_path, status: :see_other
   end
 
   private
 
   def product_params
     params.require(:product).permit(:name, :stock, :price, :expiration_date, :photo, :category)
+  end
+
+  def set_product
+    @product = Product.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+  get "products/my_products", to: "products#my_products", as: "my_products"
+  # get "products/:category", to: "products#category", as: "category"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
